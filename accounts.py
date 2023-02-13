7d6a7ac2336fbbd198ccc2829aaa5138c454171b
Shane Keenan <10012926@atu.ie>
2023-02-13 16:25:51 +0000

    pands-problem-sheet

--- accounts.py
+++ accounts.py
@@ -6,32 +6,26 @@
 #   - outputs the account number with only the last 4 digits showing
 #   - (and the first 6 digits replaced with Xs).
 # 
-# https://stackoverflow.com/questions/8761778/limiting-python-input-strings-to-certain-characters-and-lengths
+# resources used:
+#splcing -  https://www.pythonforbeginners.com/python-strings/string-splicing-in-python
+
 # author: Shane Keenan 
 
-# Status: ongoing 
+# Status: complete 
 
 
 
-#prompts user to enter a 10 digit account number assigned varible - account1 - input() defaults to a str so float() function converts to float
+#prompts user to enter a 10 digit account number assigned varible - account1 - input()- leave as a string of digits
 
-account1 = int(input("Please enter a 10 digit account number:"))
+account1 = input("Please enter a 10 digit account number:")
 
-#check if the account number is 10 digits long   
+#  splice the account number to select the last 4 digits only  
 
-elif len(account1) = 10:
-    print "Error! Only 10 characters allowed!"
-    sys.exit()
+last4_account1 = account1[6:10]
 
+# print account number with first 6 digits replaced with x's 
 
-
-
-# prints the sum together with the euro symbol 
-# checked stackoverflow again to format for 3 decimal places -  https://stackoverflow.com/questions/14540143/python-3-float-decimal-points-precision
-# THis worked nicely - so even if you say.. add 120 and 150 it will display 2.70 instead of 2.7 
-
-
-print('\nThe account number is xxxxxx {}'.format(account1))
+print('\nxxxxxx{}'.format(last4_account1))
 
 
 
